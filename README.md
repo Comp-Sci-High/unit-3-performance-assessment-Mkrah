911c8a328ed3d333aab2b7a6f9aff1c03557d17e
Mkrah <amirou.kaba26@compscihigh.org>
2026-03-05 02:51:26 +0000

    Completed all required steps

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Unit 3: Performance Assessment
 
 Why did you pick this API topic?
-
+I picked the Assassin's Creed API topic because I enjoy the games and know alot about the general lore, including the characters, factions, and historical events within the series. Creating an API around something I am interested in makes it easier and more fun to organize the data and build the project.
 
 
 Describe your API and the available data endpoints
+- Provides information about the Assassins Creed universe, including characters, factions and major events
 
+- The endpoints allow users to retrieve information about different characters and other elements of the Assassins Creed Story.
+
+/characters: Returns a list of the characters within the series and related information
+
+/characters/:id: Returning specific information about said character
 
 
 
 How was your experience building your own API server after using other APIs as a client in Unit 2?
+Making my own API helped me better understand how API's work behind the scenes. In unit too I only used API's to get data, now we're able to understand how servers send that data and handle requests. 
--- index.js
+++ index.js
@@ -1,1 +1,75 @@
 
+const express = require("express")
+const app = express()
+
+const characters = [
+  {
+    id: 0,
+    name: "Ezio Auditore",
+    era: "Renaissance Italy",
+    role: "Assassin"
+  },
+  {
+    id: 1,
+    name: "Altaïr Ibn-La'Ahad",
+    era: "Third Crusade",
+    role: "Assassin"
+  },
+  {
+    id: 2,
+    name: "Edward Kenway",
+    era: "Golden Age of Piracy",
+    role: "Pirate / Assassin"
+  }
+]
+
+// 4. Logging Middleware
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path)
+  next()
+})
+
+app.get("/", (req, res) => {
+    res.status(200).send(`
+    <h1>Assassin's Creed API</h1>
+    <p>Welcome to the Assassin's Creed API server.</p>
+    <p>Visit <a href="/docs">/docs</a> to see documentation.</p>
+  `)
+})
+
+app.get("/docs", (req, res) => {
+    res.status(200).send(`
+        <h1>API Documentation</h1>
+        <p>Available Endpoints:</p>
+        <ul>
+            <li>GET /characters -> Returns all characters</li>
+            <li>GET /characters/:id -> Returns a specific character</li>
+        </ul>
+        `)
+})
+
+app.get("/characters", (req, res) =>{
+    res.status(200).send(characters)
+})
+
+app.get("/characters/:id", (req, res)=>{
+    const id = req.params.id
+    const character = characters[id]
+
+    if (character) {
+       return res.send(200).send(character)
+    } else {
+        return res.send(404).send({error: "Character not found"})
+    }
+})
+
+app.use((req, res) => {
+    res.status(404).send(`
+        <h1>404 - Page Not Found</h1>
+        <p>The route you are looking for does not exist</p>`)
+})
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
+})
